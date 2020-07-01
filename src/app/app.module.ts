100c72ddb8274f81132d9be994c72375bfc071d0
ras.rana@vantibolli.com <ras.rana@vantibolli.com>
2020-07-01 12:49:34 +0500

    new UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialerComponent } from './dialer/dialer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialerComponent
   ],
   imports: [
     BrowserModule,
